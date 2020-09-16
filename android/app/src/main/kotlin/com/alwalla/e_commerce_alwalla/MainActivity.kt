88f886e15e6a41d23ca1a1dd024604980774f955
basharqaddah <basharqaddah@gmail.com>
2020-09-16 22:40:21 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/alwalla/e_commerce_alwalla/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alwalla.e_commerce_alwalla
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'screen/splash/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Al Walla App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
